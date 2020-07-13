f32064888f388a8c704e4a97b6040ff09bb4d64c
BohdanLiusik <bodialiusik@gmail.com>
2020-07-14 01:04:57 +0300

    Hanoi towers

---
+++ Solution/hanoi.py
@@ -0,0 +1,11 @@
+def move(n, i, k):
+    if n == 1:
+        print(n, i, k)
+    else:
+        tmp = 6 - i - k
+        move(n - 1, i, tmp)
+        print(n, i, k)
+        move(n - 1, tmp, k)
+
+n = int(input())
+move(n, 1, 3)
